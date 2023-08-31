f22543bceb5f2f7acd235bd91503a03ac94fbf53
NikodemStasiewicz <nikodemstasiewicz@gmail.com>
2023-08-31 10:50:08 +0200

    Appka

--- clock.py
+++ clock.py
@@ -1,31 +1,40 @@
-import pandas as pd
-from openpyxl import Workbook
-import os
+import requests
+import json
 
-csv_file = 'Clockify_Time_Report_Detailed_28_08_2023-03_09_2023.csv'
-df = pd.read_csv(csv_file)
+api_key = "MDQwOGNjZTktOTQ4MC00NDdlLTlmYmYtNWFhYzhjMzkwOTRk"
+workspace_id = "64ed817b29c9f301a1529730"
 
-df['Duration (timedelta)'] = pd.to_timedelta(df['Duration (h)'])
 
-pivot_df = df.pivot_table(values='Duration (timedelta)', index='User', columns='Project', aggfunc='sum', fill_value=pd.Timedelta(seconds=0))
+report_data = {
 
-pivot_df = pivot_df.apply(lambda x: x / pd.Timedelta(hours=1))
+    "dateRangeEnd": "2023-08-31T23:59:59.999Z",  # Corrected end date
 
-pivot_df['Total Hours'] = pivot_df.sum(axis=1)
+    "dateRangeStart": "2023-08-01T00:00:00.000Z", 
+    "summaryFilter": {
+        "groups": ["USER","PROJECT"],  # Group by user
+        "sortColumn": "DURATION"  # Sort by user
+    },
 
-excel_filename = 'summed_clockify_data.xlsx'
-wb = Workbook()
-ws = wb.active
-ws.title = 'Summed Clockify Data'
 
-headers = list(pivot_df.columns)
-ws.append(['User'] + headers)  
 
-for index, row in pivot_df.iterrows():
-    data_row = [index] + [row[col] for col in headers]
-    ws.append(data_row)
 
-wb.save(excel_filename)
-print(f"Summed data saved to {excel_filename}")
+}
 
-os.system(excel_filename)
+
+headers = {
+    "X-Api-Key": api_key,
+    "Content-Type": "application/json"
+}
+
+
+url = f"https://reports.api.clockify.me/v1/workspaces/{workspace_id}/reports/summary"
+
+
+response = requests.post(url, headers=headers, json=report_data)
+
+if response.status_code == 200:
+    report_result = response.json()
+    print(json.dumps(report_result, indent=2))
+else:
+    print(f"Nie udało się wygenerować raportu. Kod statusu: {response.status_code}")
+    print(response.text)  
